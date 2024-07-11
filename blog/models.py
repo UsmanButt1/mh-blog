02032c2edb4f9eb7b9543e3cac730c3b4e471d7d
Mohammad Butt <u.butt91@gmail.com>
2024-07-11 08:59:57 +0000

    Add models and migrated and updated ReadMe

--- blog/models.py
+++ blog/models.py
@@ -5,27 +5,23 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
-class JournalEntry(models.Model):
-    """
-    Journal Entry model
-    """
-    title = models.CharField(max_length=200, blank=False, null=False)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    published_on = models.DateTimeField(auto_now_add=True)
-    entry_text = models.TextField()
-    mood_id = models.ForeignKey(Mood, on_delete=models.SET_NULL, null=True)
-    resource_id = models.ForeignKey(Resource, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return f"{self.user.username}'s Entry - {self.date}"
 
 class Mood(models.Model):
     """
     Mood model
     """
-    mood = models.CharField(max_length=100)
+    MOOD_TYPES = [
+        ('H', 'Happy'),
+        ('N', 'Neutral'),
+        ('S', 'Sad'),
+    ]
+    mood = models.CharField(max_length=1, choices=MOOD_TYPES)
+    reason = models.CharField(max_length=200, null=False)
     creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"REASON: {self.reason}"
 
 class Resource(models.Model):
     """
@@ -40,3 +36,22 @@ class Resource(models.Model):
     url = models.URLField()
     resource_type = models.CharField(max_length=1, choices=RESOURCE_TYPES)
     creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"TITLE: {self.title}"
+
+class JournalEntry(models.Model):
+    """
+    Journal entry model
+    """
+    title = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_on = models.DateTimeField(auto_now_add=True)
+    entry_text = models.TextField()
+    mood = models.ForeignKey(Mood, on_delete=models.SET_NULL, null=True)
+    resource = models.ForeignKey(Resource, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"TITLE: {self.title}, ENTRY TEXT: {self.entry_text}"
